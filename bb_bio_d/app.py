13d461eb8d85166db63629d477a80381ab09f675
Mark <mflynn56@yahoo.com>
2019-03-02 23:49:54 -0800

    HW submission, final pie and bubble chart, add gauge chart'

--- bb_bio_d/app.py
+++ bb_bio_d/app.py
@@ -13,10 +13,8 @@ from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 
-
-#################################################
+#----------------------------------------------
 # Database Setup
-#################################################
 
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///db/bellybutton.sqlite"
 db = SQLAlchemy(app)
@@ -30,50 +28,63 @@ Base.prepare(db.engine, reflect=True)
 Samples_Metadata = Base.classes.sample_metadata
 Samples = Base.classes.samples
 
+#----------------------------------------------
+# Return the homepage.
 
 @app.route("/")
 def index():
-    """Return the homepage."""
 #    return '<h1> App started ... </h1>'
     return render_template("index.html")
 
-# This commentis annoying
+#----------------------------------------------
+# Return a list of sample (table column) names.
 
 @app.route("/names")
 def names():
-    """Return a list of sample (table column) names."""
+
+    # Sample table column names are available sample ids
     return jsonify(Samples.__table__.columns.keys()[2:])
+
+#----------------------------------------------
+# Return the MetaData for a given sample.
 
 @app.route("/metadata/<sample>")
 def sample_metadata(sample):
-    """Return the MetaData for a given sample."""
 
+    # Use dictionary of metadata table columns for query selection
     meta_cols = Samples_Metadata.__dict__
 
+    # List of lables and column keys for query and return data
     desc = ['Sample Id', 'Ethnicity', 'Gender', 'Age', 'Location', 'BB Type', 'Wash Freq']
     cols = ['sample', 'ETHNICITY', 'GENDER', 'AGE', 'LOCATION', 'BBTYPE', 'WFREQ']
     sel  = [meta_cols[c] for c in cols]
 
+    # Get metadata for selected subject sample
     results = db.session.query(*sel).filter(Samples_Metadata.sample == sample).first()
     
+    # Return JSON data for selected subject metadata
     return jsonify([{desc[i]:results[i] for i in range(0,len(desc))}][0])
 
+#----------------------------------------------------
+# Return `otu_ids`, `otu_labels` and `sample_values`.
 
 @app.route("/samples/<sample>")
 def samples(sample):
-    """Return `otu_ids`, `otu_labels`,and `sample_values`."""
 
+    # Select table column based on sample id
     sample_cols = Samples.__dict__
     sel = [sample_cols[c] for c in ['otu_id', 'otu_label', sample]]
 
     # Query and filter the data sample, only keep rows with values above 1
     results = db.session.query(*sel).filter(sel[2]>1).order_by(desc(sel[2])).all()
 
+    # Organize return data as arrays to facilitate plotting
     data = {'otu_id'   :[r[0] for r in results], 
             'otu_label':[r[1] for r in results], 
             'samples'  :[r[2] for r in results]}
 
+    # Return JSON for selected sample data
     return jsonify(data)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run()
--- bb_bio_d/static/js/app.js
+++ bb_bio_d/static/js/app.js
@@ -1,67 +1,152 @@
 
-var gMetaData = [];
-var gSamples = {};
-
+/*****************************************************/
+/* buildMetadata - Get metadata for selected sample
+*/
 function buildMetadata(sample) {
 
-  // @TODO: Complete the following function that builds the metadata panel
-
-  // Use `d3.json` to fetch the metadata for a sample
-    // Use d3 to select the panel with id of `#sample-metadata`
-
-    // Use `.html("") to clear any existing metadata
-
-    // Use `Object.entries` to add each key and value pair to the panel
-    // Hint: Inside the loop, you will need to use d3 to append new
-    // tags for each key-value in the metadata.
-
-    // BONUS: Build the Gauge Chart
-    // buildGauge(data.WFREQ);
-
+  // Query and recieve sample metadata, populate the 
+  // metadata table of the Dahsboard
   d3.json(`/metadata/${sample}`).then((metaData) => {
     d3.select('#meta-table').html("");  
     Object.entries(metaData).forEach(md => {
       tr = d3.select('#meta-table').append('tr');
-      tr.append('td').text(md[0]);
-      tr.append('td').text(`: ${md[1]}`)
+      tr.append('td').append('small').text(md[0]);
+      tr.append('td').append('small').text(`: ${md[1]}`)
     });
+
+  // Draw the Gauge Chart for samples wash frequency
+  buildGaugeChart (metaData['Wash Freq']);
   });
 }
 
+/*****************************************************/
+/* buildCharts - Build the Pie and Bubble charts
+*/
 function buildCharts(sample) {
 
   d3.json(`/samples/${sample}`).then(samples => {
-    gSamples = samples;
 
-    // Set up pie chart
+    // Set up pie chart for top 10 sample counts
+    // Data is recieved sorted in descending order so
+    // only need to take the first 10 values for the chart
     trace = {
-      values : samples.samples.slice(0,10),
-      labels : samples.otu_id.slice(0,10),
-      hovertext: samples.otu_label.slice(0, 10),
-      hoverinfo: "hovertext",
-      type: 'pie'
+      values    : samples.samples.slice(0,10),
+      labels    : samples.otu_id.slice(0,10).map(otu => `otu: ${otu}`),
+      hovertext : samples.otu_label.slice(0, 10),
+      hoverinfo : 'label+value+text',
+      title     : {text : 'Top 10 Sample Percentage',
+                   font : {size : 20}},
+      type      : 'pie'
     }
 
-    layout = {margin: {t:0, l:0, b:0, r:0},
-              height:400, width:550};
-    //layout = {height:600, width:600};
-    //layout = {autosize:true, automargin:true};
+    // Layout for the pie chart. I had trouble controling 
+    // height, width and position, of hard code for now.
+    layout = {margin : {t:0, l:0, b:0, r:0},
+              height : 450, width:500};
 
-    Plotly.plot('pie-chart', [trace], layout)
+    // Draw Pie Chart in center column of row
+    Plotly.newPlot('pie-chart', [trace], layout)
 
-    // Setup the bubble chart
+    // Setup the bubble chart, trace and layout objects
     trace = {
-      x: samples.otu_id,
-      y: samples.samples,
-      mode: 'markers',
-      marker: { size:samples.samples}
+      x         : samples.otu_id,
+      y         : samples.samples,
+      mode      : 'markers',
+      marker    : { size    : samples.samples,
+                    symbol  : 'circle',
+                    sizemin : 5,
+                    color   : samples.otu_id,
+                    colorscale : 'Jet'},
+      hoverinfo : 'x+y'
     }
-    layout = {title:"Scatter", height:700, width:1800}
 
-    Plotly.plot('scatter-plot',[trace], layout, {})
+    layout = {
+      title  : {text : 'Sample Count vs OTU-ID',
+                font : {size : 20}},
+      height : 700, 
+      width  : 1200
+    }
+
+    Plotly.newPlot('scatter-plot', [trace], layout)
   });
 }
 
+/*****************************************************/
+/* buildGaugeChart - Build the Gauge chart
+*/
+function buildGaugeChart (washFreq) {
+  // Scale wash frequency to 180 degrees
+  var level = (washFreq * (180/10))+5;
+
+  // Trig to calc meter point
+  var degrees = 180 - level;
+  var radius  = .5;
+  var radians = degrees * Math.PI / 180;
+
+  var x = radius * Math.cos(radians);
+  var y = radius * Math.sin(radians);
+
+  // Path: may have to change to create a better triangle
+  var mainPath = 'M -.0 -0.025 L .0 0.025 L ',
+      pathX = String(x),
+      space = ' ',
+      pathY = String(y),
+      pathEnd = ' Z';
+  var path = mainPath.concat(pathX,space,pathY,pathEnd);
+
+var values = [50/10, 50/10, 50/10, 50/10, 50/10, 50/10, 50/10, 50/10, 50/10, 50/10, 50]
+var labels = ['9', '8', '7', '6', '5', '4', '3', '2', '1', '0', ''];
+var text   = ['Diamond',  'Shinny',    'Clean',  'Sanitary', 'Average',
+              'Passable', 'Managable', 'Trying', 'Crusty',   'Narly', ''];
+
+var data = [{ 
+    type : 'scatter',
+    x : [0], y : [0],
+    marker     : {size: 28, color:'850000'},
+    showlegend : false,
+    name       : 'speed',
+    text       : level,
+    hoverinfo  : 'text+name'},
+
+  { values       : values,
+    rotation     : 90,
+    text         : labels,
+    textinfo     : 'text',
+    textposition : 'inside',
+    marker       : {colors : ['rgba(14, 127, 0, .5)',    'rgba(110, 154, 22, .5)',
+                              'rgba(170, 202, 42, .5)',  'rgba(202, 209, 95, .5)',
+                              'rgba(170, 202, 42, .5)', 'rgba(232, 226, 202, .5)',
+                              'rgba(210, 206, 145, .5)', 'rgba(232, 226, 202, .5)',
+                              'rgba(210, 206, 145, .5)', 'rgba(232, 226, 202, .5)',
+                              'rgba(155, 155, 155, 0)']},
+    labels       : text,
+    hoverinfo    : 'label',
+    hole         : .5,
+    type         : 'pie',
+    showlegend   : false
+}];
+
+var layout = {
+  shapes:[{
+    type      : 'path',
+    path      : path,
+    fillcolor : '850000',
+    line : { color : '850000'}
+    }],
+
+  title  : 'Wash Frequency Gauge',
+  height : 550,
+  width  : 600,
+  xaxis  : {zeroline:false, showticklabels:false, showgrid: false, range: [-1, 1]},
+  yaxis  : {zeroline:false, showticklabels:false, showgrid: false, range: [-1, 1]}
+};
+
+Plotly.newPlot('gauge-chart', data, layout);
+}
+
+/*****************************************************/
+/* init - Initial setup for Dashboard page
+*/
 function init() {
   // Grab a reference to the dropdown select element
   var selector = d3.select("#selDataset");
@@ -82,6 +167,9 @@ function init() {
   });
 }
 
+/*****************************************************/
+/* optionChanged - Callback for sample selection change
+*/
 function optionChanged(newSample) {
   // Fetch new data each time a new sample is selected
   buildCharts(newSample);
